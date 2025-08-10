33de6cbfaf2a9b794ffbf60f6af9988261580655
Dineth Lanka <upekadineth12@gmail.com>
2025-08-10 14:19:47 +0530

    payment process completed

---
+++ src/java/hibernate/Orders.java
@@ -0,0 +1,68 @@
+package hibernate;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="orders")
+public class Orders implements Serializable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+    
+    @ManyToOne
+    @JoinColumn(name="user_id")
+    private User user;
+    
+    @Column(name="created_at")
+    private Date createdAt;
+    
+    @ManyToOne
+    @JoinColumn(name="user_data_user_id")
+    private UserData userData;
+
+    public Orders() {
+    }   
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public UserData getUserData() {
+        return userData;
+    }
+
+    public void setUserData(UserData userData) {
+        this.userData = userData;
+    }
+    
+}
